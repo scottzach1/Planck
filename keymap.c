b79f75ee971ee97a59778a4ee586b6d4d66802b9
Zac <Scott>
2019-12-18 00:51:14 +1300

    Corrects right control key on _COLEMAK layer.

--- keymap.c
+++ keymap.c
@@ -32,7 +32,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_QUOT,
     FUNCT,   KC_A,    KC_R,    KC_S,    KC_T,    KC_G,    KC_M,    KC_N,    KC_E,    KC_I,    KC_O,    KC_ENT,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,    KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_SLSH, KC_BSLS,
-    KC_LCTL, KC_LGUI, KC_LALT, KC_LSFT, KC_SPC,  LOWER,   RAISE,   KC_BSPC, KC_RSFT, KC_MINS, KC_EQL,  KC_RGHT
+    KC_LCTL, KC_LGUI, KC_LALT, KC_LSFT, KC_SPC,  LOWER,   RAISE,   KC_BSPC, KC_RSFT, KC_MINS, KC_EQL,  KC_RCTL
 ),
 
 /* Lower
